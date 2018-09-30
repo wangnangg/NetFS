d3565e151e4bbbac56d0eed9250aaa738270c1cb
Nan Wang <wangnangg@gmail.com>
2018-09-29 21:17:45 -0400

    compatible with ubuntu 18

--- prepare.sh
+++ prepare.sh
@@ -3,13 +3,5 @@ meson build
 set -e
 pushd build
 ninja
-popd
-popd
-configs="debug release"
-for config in $configs
-do
-dir=build/$config/lib
-mkdir -p $dir
-cp fuse-3/build/lib/libfuse3.so* $dir
-done
+sudo ninja install
 
