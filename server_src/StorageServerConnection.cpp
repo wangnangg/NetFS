84b27130808772aab7ecd50e63412d1211965e3d
Nan Wang <wangnangg@gmail.com>
2018-10-18 22:43:59 -0400

    receiving actual message on server side

--- server_src/StorageServerConnection.cpp
+++ server_src/StorageServerConnection.cpp
@@ -5,10 +5,12 @@
 #include <Poco/Net/SocketStream.h>
 #include <Poco/Process.h>
 #include <Poco/StreamCopier.h>
+#include <cassert>
 #include <cstring>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include "msg.hpp"
 
 #define BUFF_SIZE 4096
 //#define NUM_REQ 16000 // for terminating server for scalability testing
@@ -23,49 +25,51 @@ StorageServerConnection::StorageServerConnection(
 
 StorageServerConnection::~StorageServerConnection() {}
 
+/* connection should be persistent. An instance should serve a client until
+ * the client shuts down.
+ */
 void StorageServerConnection::run()
 {
-    // For measuring execution time
-    if (this->count == 1)
-    {
-        this->firstRequestTime = Poco::Timestamp();
-    }
-
-    std::string response =
-        " * Storage Server received message from address: " +
-        this->socket().peerAddress().toString() + "\r\n" +
-        " * This StorageServer will reply with the requested file... "
-        "eventually \r\n" +
-        " *\r\n" + " *\r\n" + " * Seeks are bad.... \r\n";
-
     try
     {
-        // this->socket().setKeepAlive(true);
-
-        char Buffer[BUFF_SIZE];
-
-        size_t readBytes = this->socket().receiveBytes(Buffer, BUFF_SIZE);
-
-        std::cout << "Received message from address: "
-                  << this->socket().peerAddress().toString() << std::endl;
-        std::cout << readBytes << " bytes read into buffer of size "
-                  << BUFF_SIZE << std::endl;
-        std::cout << "======================================================"
-                  << std::endl;
-        std::cout << "Received message: " << std::endl;
-        std::cout << Buffer << std::endl;
-        std::cout << "======================================================"
-                  << std::endl;
+        while (true)
+        {
+            auto msg = unserializeMsg([&](char* buf, size_t size) {
+                int off = 0;
+                int left = (int)size;
+                while (left > 0)
+                {
+                    int read_size =
+                        this->socket().receiveBytes(buf + off, left);
+                    if (read_size == 0)
+                    {
+                        // client closed (for unknown reason)
+                        throw Poco::Net::NetException(
+                            "client closed with out say bye-bye. rude");
+                    }
+                    assert(read_size <= left);
+                    left -= read_size;
+                    off += read_size;
+                }
+            });
+            switch (msg->type)
+            {
+                case Msg::Open:
+                    std::cout << "MsgOpen" << std::endl;
+                    break;
+                case Msg::Stat:
+                    std::cout << "MsgClose" << std::endl;
+                    break;
+                default:
+                    std::cout << "unexpected Msg type:" << msg->type
+                              << std::endl;
+            }
+        }
     }
     catch (const Poco::Net::NetException& pne)
     {
         std::cerr << pne.what() << std::endl;
     }
-
-    // const char* to_client = response.c_str();
-
-    // size_t sentBytes = this->socket().sendBytes(to_client,
-    // strlen(to_client) + 1);
 }
 
 unsigned long StorageServerConnection::next_count = 1;
