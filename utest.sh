bcb88551268f046f55238c91fb75f1346e74cae9
Nan Wang <wangnangg@gmail.com>
2018-10-20 11:45:59 -0400

    CI

--- utest.sh
+++ utest.sh
@@ -4,8 +4,11 @@ config=${2:-debug}
 filter=${1:-*}
 
 python3 genmake.py
-echo make build/${config}/utest
-make build/${config}/utest
+echo make build/${config}/utest config=${config}
+make build/${config}/utest config=${config}
 
 echo ./build/${config}/utest --gtest_filter="$filter"
 ./build/${config}/utest --gtest_filter="$filter"
+
+echo varlgrinding unit tests
+valgrind --error-exitcode=1 ./build/${config}/utest --gtest_filter="$filter"
