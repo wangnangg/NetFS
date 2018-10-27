b92f3234b85ef19657a8a6381ee3fa64e5edcd38
Nan Wang <wangnangg@gmail.com>
2018-10-26 22:23:37 -0400

    demo scripts

--- integration_test/remote_mount.sh
+++ integration_test/remote_mount.sh
@@ -2,8 +2,9 @@
 set -e
 root=..
 int_test=.
-
+config=release
+make config=${config} -C ${root}
 tmux \
-  new-session "${root}/build/debug/client --ip=vcm-6624.vm.duke.edu --port=55555 -f -d -s -o auto_unmount ${int_test}/tmp 2>&1 | tee client_out" \; \
+  new-session "${root}/build/${config}/client --ip=vcm-6624.vm.duke.edu --port=55555 -f -d -s -o auto_unmount ${int_test}/tmp 2>&1 | tee client_out" \; \
   split-window "sleep 2.0; cd ${int_test}/tmp; bash" \; \
   select-layout even-vertical
--- integration_test/setup_server.sh
+++ integration_test/setup_server.sh
@@ -2,7 +2,7 @@
 set -e
 root=..
 int_test=.
-make -C ${root}
+make config=release -C ${root}
 mkdir -p ${int_test}/tmp
 mkdir -p ${int_test}/nfs_root
-${root}/build/debug/server 2>&1 | tee server_out
+${root}/build/release/server 2>&1 | tee server_out
